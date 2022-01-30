9b3f93f996b94449e541cea262b54558e68d72e4
jillibus <hughes_0303@man.com>
2022-01-30 12:50:16 -0500

    Upload files

--- DB scripts/create_tables.sql
+++ DB scripts/create_tables.sql
@@ -1,22 +1,26 @@
 create table label (
-  class varchar(50) primary key not null,
-  count int
-  );
-  
-create table split (
-  dataset varchar(10),
-  class varchar(50),
-  count int,
-  foreign key (class)
-    references label (class)
-  );
-  
+	index serial primary key not null,
+	class_name varchar(100)
+);
+
 create table image (
-  image_name varchar(50) not null,
-  image_location varchar(75) not null,
-  class varchar(50),
-  vehicle char(1) check (vehicle in ('Y','N')),
-  primary key (image_location, image_name),
-  foreign key (class) 
-	references label (class)
-  );
+	index serial primary key not null,
+	bbox_x1 int,
+	bbox_y1 int,
+	bbox_x2 int,
+	bbox_y2 int,
+	class	int,
+	fname	varchar(150),
+	foreign key (class)
+		references label (index)
+);
+
+-- SEQUENCE: 
+-- SEQUENCES for index columns
+
+CREATE SEQUENCE image_index_seq;
+ALTER SEQUENCE image_index_seq RESTART WITH 8144;
+
+CREATE SEQUENCE label_index_seq;
+ALTER SEQUENCE label_index_seq RESTART WITH 196;
+
